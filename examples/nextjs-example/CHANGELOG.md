d4446826fd380f7965d4e0f7e6d6308890f5516a
toan.vo24122000 <space.vtoan@gmail.com>
2023-05-16 17:54:56 +0700

    MET-162 - add example

---
+++ examples/nextjs-example/CHANGELOG.md
@@ -0,0 +1,128 @@
+# @aptos-labs/wallet-adapter-nextjs-example
+
+## 1.0.6
+
+### Patch Changes
+
+- 56a3f9f: BCS transaction support in react provider package
+- Updated dependencies [56a3f9f]
+  - @aptos-labs/wallet-adapter-react@1.0.5
+  - @aptos-labs/wallet-adapter-ant-design@1.0.3
+  - @aptos-labs/wallet-adapter-mui-design@0.3.2
+
+## 1.0.5
+
+### Patch Changes
+
+- Updated dependencies [8dea640]
+  - @aptos-labs/wallet-adapter-react@1.0.4
+  - @aptos-labs/wallet-adapter-ant-design@1.0.2
+  - @aptos-labs/wallet-adapter-mui-design@0.3.1
+
+## 1.0.4
+
+### Patch Changes
+
+- Updated dependencies [55727e0]
+  - @aptos-labs/wallet-adapter-mui-design@0.3.0
+
+## 1.0.3
+
+### Patch Changes
+
+- Updated dependencies [be2e1ac]
+  - @aptos-labs/wallet-adapter-mui-design@0.2.0
+
+## 1.0.2
+
+### Patch Changes
+
+- Updated dependencies [03eb0f5]
+  - @aptos-labs/wallet-adapter-ant-design@1.0.1
+  - @aptos-labs/wallet-adapter-mui-design@0.1.2
+  - @aptos-labs/wallet-adapter-react@1.0.3
+
+## 0.4.2
+
+### Patch Changes
+
+- Updated dependencies [e10ea7b]
+  - @aptos-labs/wallet-adapter-ant-design@1.0.0
+
+## 0.4.1
+
+### Patch Changes
+
+- Updated dependencies [2678efe]
+  - @aptos-labs/wallet-adapter-mui-design@0.1.1
+
+## 0.4.0
+
+### Minor Changes
+
+- e03f79c: Update Blocto and Martian package version and fix a minor autoConnect bug
+
+### Patch Changes
+
+- Updated dependencies [e03f79c]
+  - @aptos-labs/wallet-adapter-react@0.2.4
+
+## 0.3.0
+
+### Minor Changes
+
+- f62830b: Add MUI framework design package to the wallet adapter with updated nextjs example
+
+### Patch Changes
+
+- Updated dependencies [f62830b]
+  - @aptos-labs/wallet-adapter-mui-design@0.1.0
+
+## 0.2.2
+
+### Patch Changes
+
+- Updated dependencies [7498973]
+  - @aptos-labs/wallet-adapter-ant-design@0.1.1
+  - @aptos-labs/wallet-adapter-react@0.2.3
+
+## 0.2.1
+
+### Patch Changes
+
+- Updated dependencies [552d255]
+  - @aptos-labs/wallet-adapter-react@0.2.2
+
+## 0.2.0
+
+### Minor Changes
+
+- 8124d54: Create wallet selector modal with ant design
+
+### Patch Changes
+
+- Updated dependencies [8124d54]
+  - @aptos-labs/wallet-adapter-ant-design@0.1.0
+
+## 0.1.1
+
+### Patch Changes
+
+- c3eb031: Export WalletReadyState enum from react package
+- Updated dependencies [c3eb031]
+  - @aptos-labs/wallet-adapter-react@0.2.1
+
+## 0.1.0
+
+### Minor Changes
+
+- 6e53116: Add support to verify a signed message
+
+### Patch Changes
+
+- Updated dependencies [18a0429]
+- Updated dependencies [42e29f6]
+- Updated dependencies [576bb57]
+- Updated dependencies [6e53116]
+  - @aptos-labs/wallet-adapter-core@0.2.0
+  - @aptos-labs/wallet-adapter-react@0.2.0
---
+++ examples/nextjs-example/pages/create-event.tsx
@@ -0,0 +1,266 @@
+import { useWallet } from "@aptos-labs/wallet-adapter-react";
+import { Button, DatePicker, DatePickerProps, Input, Select } from "antd";
+import InformationSDK from "../../../src/index";
+import { useState } from "react";
+
+interface IDataInput {
+  description?: string;
+  collection?: string;
+  uri?: string;
+  reward?: number;
+  options?: string[];
+  startTime?: number;
+  endTime?: number;
+  payoutTime?: number;
+}
+const createEvent = () => {
+  const {
+    connected,
+    disconnect,
+    account,
+    network,
+    wallet,
+    signAndSubmitTransaction,
+    signAndSubmitBCSTransaction,
+    signTransaction,
+    signMessage,
+    signMessageAndVerify,
+  } = useWallet();
+  const [result, setResult] = useState();
+  const [typeEvent, setTypeEvent] = useState("predict");
+  const [coinType, setCoinType] = useState("0x1::aptos_coin::AptosCoin");
+  const [dataInput, setDateInput] = useState<IDataInput>({
+    description: "",
+    collection: "",
+    uri: "",
+    reward: 0,
+    options: [],
+    startTime: 0,
+    endTime: 0,
+    payoutTime: 0,
+  });
+
+  const informationSDk = new InformationSDK(signAndSubmitTransaction, 2);
+  let now = new Date().getTime();
+  const handleChangeTypeEvent = (value: string) => {
+    setTypeEvent(value);
+  };
+  const handleChangeCoinType = (value: string) => {
+    setCoinType(value);
+  };
+
+  const onChangeStartTime: DatePickerProps["onChange"] = (date) => {
+    setDateInput((pre: IDataInput) => {
+      return {
+        ...pre,
+        startTime: date?.valueOf(),
+      };
+    });
+  };
+
+  const onChangeEndTime: DatePickerProps["onChange"] = (date) => {
+    setDateInput((pre: IDataInput) => {
+      return {
+        ...pre,
+        endTime: date?.valueOf(),
+      };
+    });
+  };
+  const onChangePayoutTime: DatePickerProps["onChange"] = (date) => {
+    setDateInput((pre: IDataInput) => {
+      return {
+        ...pre,
+        payoutTime: date?.valueOf(),
+      };
+    });
+  };
+
+  const handleCreateEvent = () => {
+    if (typeEvent === "predict") {
+      informationSDk
+        .createPredictEvent(
+          dataInput.description!,
+          dataInput.uri!,
+          dataInput.options!,
+          dataInput.startTime!,
+          dataInput.endTime!,
+          dataInput.payoutTime!,
+          coinType
+        )
+        .then((data) => setResult(data));
+      return;
+    }
+    if (typeEvent === "survey") {
+      informationSDk
+        .createSuyveyEvent(
+          dataInput.description!,
+          dataInput.uri!,
+          dataInput.reward!,
+          dataInput.options!,
+          dataInput.startTime!,
+          dataInput.endTime!,
+          dataInput.payoutTime!,
+          coinType
+        )
+        .then((data) => setResult(data));
+    }
+    if (typeEvent === "survey-nft") {
+      informationSDk
+        .createSuyveyNFTEvent(
+          dataInput.description!,
+          dataInput.collection!,
+          dataInput.uri!,
+          dataInput.options!,
+          dataInput.startTime!,
+          dataInput.endTime!,
+          dataInput.payoutTime!
+        )
+        .then((data) => setResult(data));
+    }
+  };
+
+  const onChangeInput = (e: any) => {
+    if (e.target.name === "reward") {
+      if (e.target.value == +e.target.value) {
+        setDateInput((pre: IDataInput) => {
+          return {
+            ...pre,
+            [e.target.name]: +e.target.value,
+          };
+        });
+      }
+    }
+    setDateInput((pre: IDataInput) => {
+      return {
+        ...pre,
+        [e.target.name]: e.target.value,
+      };
+    });
+  };
+
+  const onKeyDown = (e: any) => {
+    if (e.keyCode === 13) {
+      setDateInput((pre: IDataInput) => {
+        let options = pre.options;
+        options?.push(e.target.value);
+        return {
+          ...pre,
+          options: options,
+        };
+      });
+    }
+  };
+
+  return (
+    <div className="container">
+      <div className="title">Create Event</div>
+
+      <div className="input">
+        <div className="input-label">Type Event</div>
+        <div className="input-field">
+          <Select
+            defaultValue={typeEvent}
+            onChange={handleChangeTypeEvent}
+            options={[
+              { value: "predict", label: "Predict" },
+              { value: "survey", label: "Survey" },
+              { value: "survey-nft", label: "Survey-NFT" },
+            ]}
+          />
+        </div>
+      </div>
+      <div className="input">
+        <div className="input-label">Description</div>
+        <div className="input-field">
+          <Input
+            placeholder="description"
+            name="description"
+            onChange={onChangeInput}
+            value={dataInput.description}
+          />
+        </div>
+      </div>
+      <div className="input">
+        <div className="input-label">Collection</div>
+        <div className="input-field">
+          <Input
+            placeholder="collection"
+            name="collection"
+            onChange={onChangeInput}
+            value={dataInput.collection}
+            disabled={typeEvent !== "survey-nft"}
+          />
+        </div>
+      </div>
+      <div className="input">
+        <div className="input-label">URI</div>
+        <div className="input-field">
+          <Input
+            placeholder="uri"
+            name="uri"
+            onChange={onChangeInput}
+            value={dataInput.uri}
+          />
+        </div>
+      </div>
+      <div className="input">
+        <div className="input-label">reward</div>
+        <div className="input-field">
+          <Input
+            placeholder="reward"
+            name="reward"
+            onChange={onChangeInput}
+            value={dataInput.reward}
+            disabled={typeEvent !== "survey"}
+          />
+        </div>
+      </div>
+      <div className="input">
+        <div className="input-label">Options</div>
+        <div className="input-field">
+          <Input placeholder="options" name="option" onKeyDown={onKeyDown} />
+          {dataInput?.options?.map((value, index) => (
+            <div key={index}>{value}</div>
+          ))}
+        </div>
+      </div>
+      <div className="input">
+        <div className="input-label">Start Time</div>
+        <div className="input-field">
+          <DatePicker showTime onChange={onChangeStartTime} />
+        </div>
+      </div>
+      <div className="input">
+        <div className="input-label">End Time</div>
+        <div className="input-field">
+          <DatePicker showTime onChange={onChangeEndTime} />
+        </div>
+      </div>
+      <div className="input">
+        <div className="input-label">Payout Time</div>
+        <div className="input-field">
+          <Input
+            placeholder="payout time"
+            name="payoutTime"
+            onChange={onChangeInput}
+            value={dataInput.payoutTime}
+          />
+        </div>
+      </div>
+      <div className="input">
+        <div className="input-label">Coin Type</div>
+        <div className="input-field">
+          <Select
+            defaultValue={coinType}
+            onChange={handleChangeCoinType}
+            options={[{ value: "0x1::aptos_coin::AptosCoin", label: "APT" }]}
+          />
+        </div>
+      </div>
+
+      <Button onClick={handleCreateEvent}>Create Event</Button>
+      {result && <div>{JSON.stringify(result)}</div>}
+    </div>
+  );
+};
+export default createEvent;
